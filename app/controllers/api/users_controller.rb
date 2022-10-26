346dcbea52c12aa76fe61b54d5b7821211c76e06
FranciscoRosal <ros18676@uvg.edu.gt>
2022-10-26 17:07:11 -0600

    feat: add api users controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,43 @@
+class Api::UsersController < ApplicationApiController
+    before_action :set_user, only: %i[ show edit update destroy ]
+
+    # GET /users or /users.json
+    def index
+        @users = User.all
+        respond_with_status(200, @users)
+    end
+
+    # GET /users/1 or /users/1.json
+    def show
+        respond_with_status(200, @user)
+    end
+
+    # PATCH/PUT /users/1 or /users/1.json
+    def update
+        if @user.update(user_params)
+            respond_with_status(200, "User was successfully updated.")
+        else
+            respond_with_status(400, @user.errors)
+        end
+    end
+
+    # DELETE /users/1 or /users/1.json
+    def destroy
+        @user.destroy
+
+        respond_with_status(200, "User was successfully destroyed.")
+    end
+
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+        @user = User.find_by_id(params[:id])
+        return respond_with_status(404, "User not found.") if @user.blank?
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+        params.fetch(:user, {})
+    end
+end
