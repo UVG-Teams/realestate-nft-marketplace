87e1f9a572f301b67ca1010cc7722bfe2204892c
FranciscoRosal <ros18676@uvg.edu.gt>
2022-10-26 18:48:51 -0600

    feat: add properties api controller

---
+++ app/controllers/api/properties_controller.rb
@@ -0,0 +1,58 @@
+class Api::PropertiesController < ApplicationApiController
+    before_action :set_property, only: %i[ show edit update destroy ]
+
+    # GET /properties or /properties.json
+    def index
+        @properties = Property.all
+        respond_with_status(200, @properties)
+    end
+
+    # GET /properties/1 or /properties/1.json
+    def show
+        respond_with_status(200, @property)
+    end
+
+    # POST /properties or /properties.json
+    def create
+        @property = Property.new(property_params)
+
+        if @property.save
+            respond_with_status(200, "Property was successfully created.")
+        else
+            respond_with_status(400, @property.errors)
+        end
+    end
+
+    # PATCH/PUT /properties/1 or /properties/1.json
+    def update
+        return respond_with_status(401) if @property.user != @current_user
+
+        if @property.update(property_params)
+            respond_with_status(200, "Property was successfully updated.")
+        else
+            respond_with_status(400, @property.errors)
+        end
+    end
+
+    # DELETE /properties/1 or /properties/1.json
+    def destroy
+        return respond_with_status(401) if @property.user != @current_user
+
+        @property.destroy
+
+        respond_with_status(200, "Property was successfully destroyed.")
+    end
+
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_property
+        @property = Property.find_by_id(params[:id])
+        return respond_with_status(404, "Property not found.") if @property.blank?
+    end
+
+    # Only allow a list of trusted parameters through.
+    def property_params
+        params.fetch(:property, {})
+    end
+end
