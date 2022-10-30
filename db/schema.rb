b4137d4ef6db20315d82c4384969184c45397341
FranciscoRosal <ros18676@uvg.edu.gt>
2022-10-29 19:20:11 -0600

    feat: user additional info

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_29_073239) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_30_011627) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_29_073239) do
     t.datetime "reset_password_sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "telephone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
