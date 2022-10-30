b6c56b92e1f2612d0b1c08d5bcc776a46ef94b4d
FranciscoRosal <ros18676@uvg.edu.gt>
2022-10-29 19:26:45 -0600

    feat: user additional info

--- db/migrate/20221030011627_alter_user_1.rb
+++ db/migrate/20221030011627_alter_user_1.rb
@@ -3,5 +3,6 @@ class AlterUser1 < ActiveRecord::Migration[7.0]
         add_column :users, :first_name, :string
         add_column :users, :last_name, :string
         add_column :users, :telephone, :string
+        add_column :users, :pid_number, :string # Personal ID Number
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_30_011627) do
     t.string "first_name"
     t.string "last_name"
     t.string "telephone"
+    t.string "pid_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
