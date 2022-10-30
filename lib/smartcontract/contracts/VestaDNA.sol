3b8c3746ced6d62fc35faaf28be5101b3c9e4e74
LucaBia <gian.luca.99@hotmail.com>
2022-10-30 14:54:30 -0600

    refactor: house struct

--- lib/smartcontract/contracts/VestaDNA.sol
+++ lib/smartcontract/contracts/VestaDNA.sol
@@ -4,21 +4,26 @@ pragma solidity ^0.8.0;
 
 contract VestaDNA {
     struct House{
-        // uint batch_num;
-        uint256 libro;
+        string typology;
+        uint256 finca;
         uint256 folio;
+        uint256 libro;
+        uint256 yearBuilt;
+        bool rebuilt;
         uint256 rooms;
-        int bathrooms;
+        uint256 bathrooms;
+        uint256 levels;
+        uint256 parkings;
+        bool yard;
+        bool pool;
         string location;
-        // address user;
-        // string timestamp;
     }
 
     House userI;
 
-    function set_house_detail(uint256 l, uint256 f, uint256 r, int b, string memory loc) public {
-        userI = House(l, f, r, b, loc);
-    }
+    // function set_house_detail(uint256 libro, uint256 folio, uint256 finca, uint256 r, int b, string memory loc) public {
+    //     userI = House(l, f, r, b, loc);
+    // }
 
     // function get_house_info() public view returns (int, int, int, int, string memory) { 
     //     return(userI.libro, userI.folio, userI.rooms, userI.bathrooms, userI.location); 
