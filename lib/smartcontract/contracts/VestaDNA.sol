f9e485f5969e756628c6e2c5eb8bf0c0ebedc1e2
LucaBia <gian.luca.99@hotmail.com>
2022-10-30 14:59:18 -0600

    refactor: set house detail

--- lib/smartcontract/contracts/VestaDNA.sol
+++ lib/smartcontract/contracts/VestaDNA.sol
@@ -21,9 +21,37 @@ contract VestaDNA {
 
     House userI;
 
-    // function set_house_detail(uint256 libro, uint256 folio, uint256 finca, uint256 r, int b, string memory loc) public {
-    //     userI = House(l, f, r, b, loc);
-    // }
+    function set_house_detail(
+        string memory typology,
+        uint256 finca,
+        uint256 folio,
+        uint256 libro,
+        uint256 yearBuilt,
+        bool rebuilt,
+        uint256 rooms,
+        uint256 bathrooms,
+        uint256 levels,
+        uint256 parkings,
+        bool yard,
+        bool pool,
+        string memory location
+    ) public {
+        userI = House(
+            typology,
+            finca,
+            folio,
+            libro,
+            yearBuilt,
+            rebuilt,
+            rooms,
+            bathrooms,
+            levels,
+            parkings,
+            yard,
+            pool,
+            location
+        );
+    }
 
     // function get_house_info() public view returns (int, int, int, int, string memory) { 
     //     return(userI.libro, userI.folio, userI.rooms, userI.bathrooms, userI.location); 
