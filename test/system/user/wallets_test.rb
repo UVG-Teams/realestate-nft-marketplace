eff5e4471172866b8210182d4762ab7272b4781b
FranciscoRosal <ros18676@uvg.edu.gt>
2022-10-30 19:08:13 -0600

    test: working with system tests

--- test/system/user/wallets_test.rb
+++ test/system/user/wallets_test.rb
@@ -3,41 +3,41 @@
 require 'application_system_test_case'
 
 class User::WalletsTest < ApplicationSystemTestCase
-  setup do
-    @user_wallet = user_wallets(:one)
-  end
+    setup do
+        @user_wallet = user_wallets(:one)
+    end
 
 =begin
-  test 'visiting the index' do
-    visit user_wallets_url
-    assert_selector 'h1', text: 'Wallets'
-  end
+    test 'visiting the index' do
+        visit user_wallets_url
+        assert_selector 'h1', text: 'Wallets'
+    end
 
-  test 'should create wallet' do
-    visit user_wallets_url
-    click_on 'New wallet'
+    test 'should create wallet' do
+        visit user_wallets_url
+        click_on 'New wallet'
 
-    click_on 'Create Wallet'
+        click_on 'Create Wallet'
 
-    assert_text 'Wallet was successfully created'
-    click_on 'Back'
-  end
+        assert_text 'Wallet was successfully created'
+        click_on 'Back'
+    end
 
-  test 'should update Wallet' do
-    visit user_wallet_url(@user_wallet)
-    click_on 'Edit this wallet', match: :first
+    test 'should update Wallet' do
+        visit user_wallet_url(@user_wallet)
+        click_on 'Edit this wallet', match: :first
 
-    click_on 'Update Wallet'
+        click_on 'Update Wallet'
 
-    assert_text 'Wallet was successfully updated'
-    click_on 'Back'
-  end
+        assert_text 'Wallet was successfully updated'
+        click_on 'Back'
+    end
 
-  test 'should destroy Wallet' do
-    visit user_wallet_url(@user_wallet)
-    click_on 'Destroy this wallet', match: :first
+    test 'should destroy Wallet' do
+        visit user_wallet_url(@user_wallet)
+        click_on 'Destroy this wallet', match: :first
 
-    assert_text 'Wallet was successfully destroyed'
-  end
+        assert_text 'Wallet was successfully destroyed'
+    end
 =end
 end
--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -3,41 +3,41 @@
 require 'application_system_test_case'
 
 class UsersTest < ApplicationSystemTestCase
-  setup do
-    @user = users(:one)
-  end
+    setup do
+        @user = users(:one)
+    end
 
 =begin
-  test 'visiting the index' do
-    visit users_url
-    assert_selector 'h1', text: 'Users'
-  end
+    test 'visiting the index' do
+        visit users_url
+        assert_selector 'h1', text: 'Users'
+    end
 
-  test 'should create user' do
-    visit users_url
-    click_on 'New user'
+    test 'should create user' do
+        visit users_url
+        click_on 'New user'
 
-    click_on 'Create User'
+        click_on 'Create User'
 
-    assert_text 'User was successfully created'
-    click_on 'Back'
-  end
+        assert_text 'User was successfully created'
+        click_on 'Back'
+    end
 
-  test 'should update User' do
-    visit user_url(@user)
-    click_on 'Edit this user', match: :first
+    test 'should update User' do
+        visit user_url(@user)
+        click_on 'Edit this user', match: :first
 
-    click_on 'Update User'
+        click_on 'Update User'
 
-    assert_text 'User was successfully updated'
-    click_on 'Back'
-  end
+        assert_text 'User was successfully updated'
+        click_on 'Back'
+    end
 
-  test 'should destroy User' do
-    visit user_url(@user)
-    click_on 'Destroy this user', match: :first
+    test 'should destroy User' do
+        visit user_url(@user)
+        click_on 'Destroy this user', match: :first
 
-    assert_text 'User was successfully destroyed'
-  end
+        assert_text 'User was successfully destroyed'
+    end
 =end
 end
