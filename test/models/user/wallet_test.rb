856f7fabb834c5453caedbde3a0ff12a873c6cf1
FranciscoRosal <ros18676@uvg.edu.gt>
2022-10-30 19:07:55 -0600

    test: working with model tests

--- test/models/user/wallet_test.rb
+++ test/models/user/wallet_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class User::WalletTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    # test "the truth" do
+    #   assert true
+    # end
 end
