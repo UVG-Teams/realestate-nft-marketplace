2d42eb98feee119fe295850bce94972d9b305e29
Marco Fuentes <mfuentes@stsa.info>
2022-11-01 21:42:13 -0600

    feat: Models loaded in jupyter notebooks

---
+++ lib/data_algorithms/image_recognition/images_list.py
@@ -0,0 +1,14 @@
+images_array = [
+    './test_real_images/bathroom_google.jpg',
+    './test_real_images/bathroom.png',
+    './test_real_images/bedroom_real.jpeg',
+    './test_real_images/bedroom_real_1.jpeg',
+    './test_real_images/garden.jpg',
+    './test_real_images/street.jpg',
+    './test_real_images/dining.png',
+    './test_real_images/kitchen_1.png',
+    './test_real_images/landscape.jpg',
+    './test_real_images/office.jpg',
+    './test_real_images/car.jpg',
+    './test_real_images/mall.png',
+]
