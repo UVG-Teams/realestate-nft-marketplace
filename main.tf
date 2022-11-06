92f8e74da5d88713c27b84ea53db15dcf5a7fc28
FranciscoRosal <ros18676@uvg.edu.gt>
2022-11-06 17:11:19 -0600

    ci: testing cd

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-    region  = "eu-west-2a"
+    region  = "us-east-1"
     # access_key = var.aws_access_key
     # secret_key = var.aws_secret_key
 }
