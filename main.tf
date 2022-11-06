a773f1dd722c9fd72fa2d7dad86fe520aad711e9
FranciscoRosal <ros18676@uvg.edu.gt>
2022-11-06 14:16:59 -0600

    ci: testing cd

--- main.tf
+++ main.tf
@@ -20,15 +20,15 @@ terraform {
     required_version = ">= 1.2.0"
 }
 
-# variable "aws_access_key" {}
-# variable "aws_secret_key" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 
 
 # Configure the AWS Provider
 provider "aws" {
     region  = "eu-west-2a"
-    # access_key = var.aws_access_key
-    # secret_key = var.aws_secret_key
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
 }
 
 
