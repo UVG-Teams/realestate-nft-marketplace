ee9c5b0141a32fc52e586906e300c81ce8c72783
FranciscoRosal <ros18676@uvg.edu.gt>
2022-11-07 00:39:10 -0600

    ci: working on server script

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 # https://developer.hashicorp.com/terraform/tutorials/aws-get-started/aws-build#write-configuration
 terraform {
-    backend "remote" {
-        # The name of your Terraform Cloud organization.
-        organization = "uvg-teams"
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "uvg-teams"
 
-        # The name of the Terraform Cloud workspace to store Terraform state files in.
-        workspaces {
-            name = "realestate-nft-marketplace"
-        }
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "realestate-nft-marketplace"
     }
+  }
 
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 4.16"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
+  }
 
-    required_version = ">= 1.2.0"
+  required_version = ">= 1.2.0"
 }
 
 variable "db_password" {}
@@ -25,7 +25,7 @@ variable "db_password" {}
 
 # Configure the AWS Provider
 provider "aws" {
-    region  = "us-east-1"
+  region = "us-east-1"
 }
 
 # AWS Lightsail Static IP
@@ -37,14 +37,14 @@ provider "aws" {
 # AWS Lightsail Database
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lightsail_database
 resource "aws_lightsail_database" "vesta-db-cd" {
-    name                 = "vesta-db-cd"
-    availability_zone    = "us-east-1a"
-    master_database_name = "database-cd"
-    master_username      = "database-cd-admin"
-    master_password      = var.db_password
-    blueprint_id         = "postgres_12"
-    bundle_id            = "micro_1_0"
-    apply_immediately    = true
+  name                 = "vesta-db-cd"
+  availability_zone    = "us-east-1a"
+  master_database_name = "database-cd"
+  master_username      = "database-cd-admin"
+  master_password      = var.db_password
+  blueprint_id         = "postgres_12"
+  bundle_id            = "micro_1_0"
+  apply_immediately    = true
 }
 
 
@@ -52,11 +52,11 @@ resource "aws_lightsail_database" "vesta-db-cd" {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lightsail_instance
 
 resource "aws_lightsail_instance" "example_test" {
-    name                = "vesta-cd"
-    availability_zone   = "us-east-1a"
-    blueprint_id        = "ubuntu_20_04"
-    bundle_id           = "small_2_0"
-    key_pair_name       = "lightsail-servers-test"
-    public_ip_address   = "34.193.107.238"
-    is_static_ip        = true
+  name              = "vesta-cd"
+  availability_zone = "us-east-1a"
+  blueprint_id      = "ubuntu_20_04"
+  bundle_id         = "small_2_0"
+  key_pair_name     = "lightsail-servers-test"
+  public_ip_address = "34.193.107.238"
+  is_static_ip      = true
 }
